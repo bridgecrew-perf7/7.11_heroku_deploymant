8b2634c159b51382b308b2e3ed9e3dfc188ec47e
yaakov_sahale <yaakov.sahale@gmail.com>
2021-11-07 12:05:43 +0200

    2nd commit

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
-const express = require(`express`)
-const APP = express()
-const PORT = 8080
+const express = require(`express`);
+const APP = express();
+const PORT = process.env.PORT;
+require(`dotenv`).config();
 
-APP.get("*",(req,res)=>{
-    res.send(`hello home page`)
-})
+APP.get("*", (req, res) => {
+  res.send(`hello home page`);
+});
 
-
-APP.listen(PORT,()=>{
-    console.log(`server listens on port: ${PORT}`);
-})
+APP.listen(PORT, () => {
+  console.log(`server listens on port: ${PORT}`);
+});
