5822caed7ecdbcdc43e2164406c7d72a0f5aa749
yaakov_sahale <yaakov.sahale@gmail.com>
2021-11-07 12:16:08 +0200

    3

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 const express = require(`express`);
-const APP = express();
-const PORT = process.env.PORT;
 require(`dotenv`).config();
+const APP = express();
+
+const PORT = process.env.PORT;
 
 APP.get("*", (req, res) => {
-  res.send(`hello home page`);
+    res.send(`hello home page`);
 });
+
 
 APP.listen(PORT, () => {
   console.log(`server listens on port: ${PORT}`);
